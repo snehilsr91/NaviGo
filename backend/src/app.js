335e3f86e9e4cced6e1fdac0c3613ebbcd61f9d1
sriharigacharya <sriharigacharya@gmail.com>
2025-11-10 20:38:33 +0530

    Changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Serve uploaded files statically
